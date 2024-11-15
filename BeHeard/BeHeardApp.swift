c417a5637de9d20cc6655f88ae001701f1d19ec3
Ed Arenberg <arenberg@epage.com>
2024-11-15 13:23:50 -0800

    Initial Commit

---
+++ BeHeard/BeHeardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeHeardApp.swift
+//  BeHeard
+//
+//  Created by Edward Arenberg on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BeHeardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeHeardTests/BeHeardTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeHeardTests.swift
+//  BeHeardTests
+//
+//  Created by Edward Arenberg on 11/15/24.
+//
+
+import Testing
+@testable import BeHeard
+
+struct BeHeardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
